6d37479b3cbc1a190fc84a113f27046a4ca8a062
Baruc <3baruc69@gmail.com>
2025-07-10 18:44:34 -0600

    pasar de menu principal a vista alumnos

---
+++ SGRP/BD1I.sql
@@ -0,0 +1,27 @@
+create table persona(
+ id_persona int primary key,
+ nombre varchar(50),
+ ap_paterno varchar(50),
+ ap_materno varchar(50)
+ );
+ 
+ create table roles(
+ id_rol int primary key,
+ nombre_rol varchar(15));
+ 
+ create table usuario(
+ id_usuario int primary key,
+ nombre_usuario varchar(50),
+ contraseña varchar(8),
+ fk_persona int,
+ fk_rol int,
+ foreign key (fk_persona) references persona(id_persona),
+ foreign key (fk_rol) references roles(id_rol));
+ 
+ create table alumno(
+ id_alumno int primary key,
+ n_control varchar(9),
+ telefono varchar(10),
+ fk_persona int,
+ foreign key (fk_persona) references persona(id_persona));
+ 
---
+++ SGRP/src/main/java/Vista/opcionAlumno2.java
@@ -0,0 +1,101 @@
+package Vista;
+
+public class opcionAlumno2 extends javax.swing.JPanel {
+
+    public opcionAlumno2() {
+        initComponents();
+    }
+
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        panelAlumnos = new javax.swing.JPanel();
+        lblCerrarSesion = new javax.swing.JLabel();
+        lblAtras = new javax.swing.JLabel();
+        lblImportar = new javax.swing.JLabel();
+        lblAgregarAlumno = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jScrollPane1 = new javax.swing.JScrollPane();
+
+        setLayout(new java.awt.BorderLayout());
+
+        lblCerrarSesion.setText("Cerrar sesión");
+
+        lblAtras.setText("Atrás");
+
+        lblImportar.setText("Importar alumnos");
+
+        lblAgregarAlumno.setText("Agregar un alumno");
+
+        jScrollPane2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255)));
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane2.setViewportView(jTable1);
+
+        jScrollPane1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255)));
+
+        javax.swing.GroupLayout panelAlumnosLayout = new javax.swing.GroupLayout(panelAlumnos);
+        panelAlumnos.setLayout(panelAlumnosLayout);
+        panelAlumnosLayout.setHorizontalGroup(
+            panelAlumnosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelAlumnosLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(panelAlumnosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(panelAlumnosLayout.createSequentialGroup()
+                        .addComponent(lblImportar)
+                        .addGap(18, 18, 18)
+                        .addComponent(lblAgregarAlumno)
+                        .addGap(18, 159, Short.MAX_VALUE)
+                        .addComponent(lblAtras)
+                        .addGap(80, 80, 80)
+                        .addComponent(lblCerrarSesion))
+                    .addGroup(panelAlumnosLayout.createSequentialGroup()
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 278, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jScrollPane1)))
+                .addContainerGap())
+        );
+        panelAlumnosLayout.setVerticalGroup(
+            panelAlumnosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelAlumnosLayout.createSequentialGroup()
+                .addGap(17, 17, 17)
+                .addGroup(panelAlumnosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblCerrarSesion)
+                    .addComponent(lblAtras)
+                    .addComponent(lblAgregarAlumno)
+                    .addComponent(lblImportar))
+                .addGap(67, 67, 67)
+                .addGroup(panelAlumnosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 336, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1))
+                .addContainerGap(63, Short.MAX_VALUE))
+        );
+
+        add(panelAlumnos, java.awt.BorderLayout.CENTER);
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JLabel lblAgregarAlumno;
+    private javax.swing.JLabel lblAtras;
+    private javax.swing.JLabel lblCerrarSesion;
+    private javax.swing.JLabel lblImportar;
+    private javax.swing.JPanel panelAlumnos;
+    // End of variables declaration//GEN-END:variables
+}
