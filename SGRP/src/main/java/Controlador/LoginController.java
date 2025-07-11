f178ca616d2f6803e2aa09f02f3d757776cd272d
Baruc <3baruc69@gmail.com>
2025-07-11 10:28:26 -0600

    Base conectada al login

---
+++ SGRP/src/main/java/Controlador/LoginController.java
@@ -0,0 +1,37 @@
+package controlador;
+
+import modelo.LoginDAO;
+import Vista.MenuPrincipal;
+import javax.swing.JOptionPane;
+import vista.Login1;
+
+public class LoginController {
+    private final LoginDAO loginDAO;
+
+    public LoginController() {
+        this.loginDAO = new LoginDAO();
+    }
+
+    public void validarCredenciales(String usuario, String contrasena, javax.swing.JFrame vista) {
+      /*  if(usuario.equals("Baruc Elorza")){
+            
+            Login1 l = new Login1();
+            l.dispose();
+        }*/
+        
+        if (usuario.isEmpty() || contrasena.isEmpty()) {
+            JOptionPane.showMessageDialog(vista, "⚠️ Todos los campos son obligatorios.");
+            return;
+        }
+        
+        String nombre = loginDAO.autenticarUsuario(usuario, contrasena);
+        if (nombre != null) {
+            MenuPrincipal p = new MenuPrincipal();
+            p.setVisible(true);
+            vista.dispose();
+        } else {
+            JOptionPane.showMessageDialog(vista, "❌ Usuario o contraseña incorrectos");
+        }
+    }
+    
+}
---
+++ SGRP/src/main/java/Modelo/LoginDAO.java
@@ -0,0 +1,29 @@
+
+package modelo;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author emilio
+ */
+public class LoginDAO {
+    public String autenticarUsuario(String usuario, String contrasena) {
+        try (Connection conn = Controlador.Conexion.obtenerConexion()) {
+            String sql = "SELECT nombre_usuario FROM usuario WHERE nombre_usuario = ? AND contraseña = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, usuario);
+            stmt.setString(2, contrasena);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                return rs.getString("nombre_usuario"); // éxito
+            }
+        } catch (SQLException e) {
+            System.err.println("Error de conexión: " + e.getMessage());
+        }
+        return null; // falló
+    }
+}
