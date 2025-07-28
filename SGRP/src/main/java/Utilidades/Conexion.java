babac830d946edbfb75dd71889eabef15897499d
EdRyz <anaylalo60@gmail.com>
2025-07-28 11:14:52 -0600

    bajando cambios

--- SGRP/src/main/java/Utilidades/Conexion.java
+++ SGRP/src/main/java/Utilidades/Conexion.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Conexion {
     private static final String URL = "jdbc:mysql://localhost:3306/sgrp?serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "E@e$l@c@d3m7s1";
+    private static final String PASSWORD = "admin";
     
     //contraseña emilio: 090774
     //contraseña yahir: manzana
